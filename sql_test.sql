060d8dee3bc4147aa68808c8e51aceb667481292
20181134 <91303066+20181134@users.noreply.github.com>
2021-10-19 19:21:57 +0900

    Update sql_test.sql

--- sql_test.sql
+++ sql_test.sql
@@ -1,12 +1,12 @@
-drop database if exists test;
-create database test character set utf8 collate utf8_general_ci;
-grant all on test.* to 'admin'@'localhost' identified by 'password';
+drop database if exists test; -- testが存在したら削除
+create database test character set utf8 collate utf8_general_ci; -- testをUTF-8で作成
+grant all on test.* to 'admin'@'localhost' identified by 'password'; -- adminに権限を付与(パスワード: password)
 use test;
 
-create table contents (
+create table contents ( -- contents データベースの作成
     id int auto_increment primary key,
-    name varchar(50) not null,
-    test1 int not null
+    name varchar(50) not null, -- ファイルの最大の大きさを50とする(空のファイルは送信できない)
+    test1 int not null -- test1を定義
 );
 
-insert into contents values(null, 'テスト', 100);
+insert into contents values(null, 'テスト', 100); -- contents にデータを入力
